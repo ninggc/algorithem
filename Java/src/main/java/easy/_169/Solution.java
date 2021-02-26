c8e9dba182fa67525f196e926f3c577ccff07de2
ninggc <ninggc@foxmail.com>
2021-02-26 16:01:55 +0800

    _169 version 1 排序后找中间的数字

--- Java/src/main/java/easy/_169/Solution.java
+++ Java/src/main/java/easy/_169/Solution.java
@@ -4,32 +4,8 @@ import java.util.Arrays;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
         Arrays.sort(nums);
-
-        int currentCount = 1;
-        int currentValue = nums[0];
-
-        int maxCount = currentCount;
-        int maxValue = currentValue;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == currentValue) {
-                ++currentCount;
-            } else {
-                if (currentCount > maxCount) {
-                    maxCount = currentCount;
-                    maxValue = currentValue;
-                }
-
-                currentCount = 1;
-                currentValue = nums[i];
-            }
-        }
-
-        return currentCount > maxCount ? currentValue : maxValue;
+        return nums[nums.length / 2];
     }
 
     public static void main(String[] args) {
