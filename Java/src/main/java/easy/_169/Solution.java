79b2e9098fd99178d7d064d93e228f72f6fe06f7
ninggc <ninggc@foxmail.com>
2021-02-26 16:40:41 +0800

    _169 version 2 摩尔乱斗法(运行之后速度较慢，不如排序取中间值)

--- Java/src/main/java/easy/_169/Solution.java
+++ Java/src/main/java/easy/_169/Solution.java
@@ -4,12 +4,40 @@ import java.util.Arrays;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        Integer[] battleField = new Integer[nums.length];
+        int battleFieldLength = 1;
+
+        battleField[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int v = nums[i];
+            for (int j = 0; j <= battleFieldLength; j++) {
+                if (j == battleFieldLength) {
+                    battleField[j] = v;
+                    ++battleFieldLength;
+                    break;
+                }
+
+                if (battleField[j] != v) {
+                    battleField[j] = battleField[battleFieldLength - 1];
+                    battleField[battleFieldLength - 1] = null;
+                    --battleFieldLength;
+                    break;
+                }
+
+            }
+        }
+
+        for (int i = 0; i < battleField.length; i++) {
+            if (battleField[i] != null) {
+                return battleField[i];
+            }
+        }
+
+        return 0;
     }
 
     public static void main(String[] args) {
-        int element = new Solution().majorityElement(new int[]{3, 3, 4});
+        int element = new Solution().majorityElement(new int[]{6,6,6, 6,6,7,7});
         System.out.println(element);
     }
 }
