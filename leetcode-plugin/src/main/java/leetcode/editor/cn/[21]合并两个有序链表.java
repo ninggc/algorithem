f5ed000ec9d077a1a7f547f8d20b09e959212689
ninggc <ning.gaocong@xinheyun.com>
2022-03-19 09:57:16 +0800

    feat: [21]合并两个有序链表.java

--- leetcode-plugin/src/main/java/leetcode/editor/cn/[21]合并两个有序链表.java
+++ leetcode-plugin/src/main/java/leetcode/editor/cn/[21]合并两个有序链表.java
@@ -82,22 +82,25 @@ class Solution {
         return result.next;
     }
 
-//    public class ListNode {
-//
-//        int val;
-//        ListNode next;
-//
-//        ListNode() {
-//        }
-//
-//        ListNode(int val) {
-//            this.val = val;
-//        }
-//
-//        ListNode(int val, ListNode next) {
-//            this.val = val;
-//            this.next = next;
-//        }
-//    }
+    /**
+     * not submit this to leetcode
+     */
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
